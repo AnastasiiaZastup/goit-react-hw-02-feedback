9257353b2fe9dc822acc32460ad684ac560eb04d
AnastasiaZastup <zastupanastasia7@gmail.com>
2023-11-28 18:29:11 +0200

    add all but not styled

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,10 @@
-import { Component } from "react";
+import React, { Component } from 'react';
+import { Statistics } from './Statistics/Statistics';
+import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
+import { Section } from './Section/Section';
+import { Notofication } from './Notofication/Notofication';
 
-class App extends Component {
+export class App extends Component {
   
   state = {
     good: 0,
@@ -8,17 +12,48 @@ class App extends Component {
     bad: 0
   }
   
-  updateClicks = () => {              //скидуємо всі стани до 0
+  updateClicks = (option) => {              
     this.setState(prevState => {
       return {
-        clicks: prevState.clicks + 1,
+        [option]: prevState[option] + 1,
       };
     });
   }
 
-  render() {
-    
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  }
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    if (total !== 0) {
+      return (good / total) * 100;
+    }
   }
 
 
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const positivefeedback = this.countPositiveFeedbackPercentage();
+
+    return (
+      <div>
+        <Section title="Please leave feedback">
+          <FeedbackOptions options={Object.keys(this.state)} onLeaveFeedback={this.updateClicks} />
+          </Section>
+         <Section title="Statistics">
+          {total > 0 ? (
+            <Statistics
+              good={good} neutral={neutral} bad={bad} total={total} positivePercentage={positivefeedback}
+            />
+          ) : (
+            <Notofication message={"There is no feedback"} />
+          )}
+       </Section>
+      </div>
+    );
+  }
 }
---
+++ src/components/Notofication/Notofication.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Notofication = ({ message }) => {
+    return <p>{message}</p>;
+};
